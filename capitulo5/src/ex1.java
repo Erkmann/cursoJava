7c63dc773c5878cc256dc3f14cc89e5073609a0d
Erkmann <erkmann08@gmail.com>
2019-12-26 16:32:58 -0300

    exe1

---
+++ capitulo5/src/ex1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        n = sc.nextInt();
+
+        if (n < 0){
+            System.out.printf("NEGATIVO");
+        }
+        else {
+            System.out.printf("POSITIVO");
+        }
+
+        sc.close();
+
+    }
+}
